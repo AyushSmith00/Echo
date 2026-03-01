ccb136c3d3cd52f812cc7d011a5cf5f431171f35
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2026-03-01 15:52:00 +0530

    protected server routes

--- backend/app/routes/server.py
+++ backend/app/routes/server.py
@@ -1,14 +1,18 @@
 from fastapi import APIRouter, Depends
 from app.crud.server import create_server
 from app.db.deps import get_db
+from sqlalchemy.orm import Session
+from app.core.dependencies import get_current_user
 
 router = APIRouter()
 
 @router.post("/servers")
-def create_server_route(name: str, db=Depends(get_db)):
-    user_id = 1
-
-    server = create_server(db, name, user_id)
+def create_server_route(
+    name: str,
+    db: Session = Depends(get_db),
+    current_user = Depends(get_current_user)
+):
+    server = create_server(db, name, current_user.id)
 
     return{
         "id": server.id,
